4fbdc42eb3cd0421273a1d9fc138831735e3e703
ykaurarshpreet <ykaur.arshpreet@gmail.com>
2025-12-10 20:39:46 -0800

    updated taskroutes file

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -12,33 +12,32 @@ taskRouter.use(authMiddleware);
  * GET /api/projects/:projectId/tasks
  */
 
-// taskRouter.get("/", getAllTasks );
-taskRouter.get("/projects/:projectId/tasks", getAllTasks);
+taskRouter.get("/", getAllTasks );
+
 
 
 /**
  * Get /api/projects/:projectId/tasks/:taskId
  */
-// taskRouter.get("/:taskId", getTaskById)
-taskRouter.get("/projects/:projectId/tasks/:taskId", getTaskById);
+taskRouter.get("/:taskId", getTaskById)
+
 
 /**
  * POST /api/projects/:projectId/tasks
  */
-// taskRouter.post("/", createTask )
-taskRouter.post("/projects/:projectId/tasks", createTask);
+taskRouter.post("/", createTask )
+
 
 
 /**
  * PUT /api/projects/:projectId/tasks/taskId
  */
 
-// taskRouter.put('/:taskId', updateTask)
-taskRouter.put("/projects/:projectId/tasks/:taskId", updateTask);
+taskRouter.put('/:taskId', updateTask)
+
 /**
  * DELETE /api/projects/:projectId/tasks/taskId
  */
-// taskRouter.delete('/:taskId', deleteTask)
-taskRouter.delete("/projects/:projectId/tasks/:taskId", deleteTask);
+taskRouter.delete('/:taskId', deleteTask)
 
 module.exports = taskRouter
